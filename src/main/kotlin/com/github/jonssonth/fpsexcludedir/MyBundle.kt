9f2020421da78e0bfc3a696c0303509dbbc25490
GitHub Action <action@github.com>
2022-03-07 08:12:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/jonssonth/fpsexcludedir/MyBundle.kt
+++ src/main/kotlin/com/github/jonssonth/fpsexcludedir/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jonssonth.fpsexcludedir
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jonssonth/fpsexcludedir/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jonssonth/fpsexcludedir/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonssonth.fpsexcludedir.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jonssonth.fpsexcludedir.MyBundle
 
 class MyProjectService(project: Project) {
 
